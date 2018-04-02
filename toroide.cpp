6b89c3a4d2634e1e7bbbc6f28119a3802d26f71e
Victor UAS <38020758+victormanu600@users.noreply.github.com>
2018-04-02 17:58:53 -0600

    toroide 1.1
    En esta version se piden como entrada el radio menor y mayor del toroide.

--- toroide.cpp
+++ toroide.cpp
@@ -6,17 +6,22 @@
 #include <iostream>
 
 #define pi 3.1415926535897932384
+float r, R;
 
 FILE *dat;
 
 int main(){
     int i,j,k;
+    printf("Introduzca el radio mayor");
+    scanf("%f",R);
+    printf("Introduzca el radio menor");
+    scanf("%f",r);
     dat = fopen("toroide.txt","w");
     fprintf(dat," \"x\",\"y\",\"value\"\n");
-    for(i=-10*13.5;i<=10*13.5;i++){
-        for(j=-10*13.5;j<=10*13.5;j++){
-            if((10-pow(pow(i/10.0,2)+pow(j/10.0,2),0.5),2>=0)&&(12.25-pow(10-pow(pow(i/10.0,2)+pow(j/10.0,2),0.5),2)>=0)){
-                    fprintf(dat,"%f,%f,%f\n",i/10.0,j/10.0,pow(12.25-pow(10-pow(pow(i/10.0,2)+pow(j/10.0,2),0.5),2), 0.5));
+    for(i=-10*(r+R);i<=10*(r+R);i++){
+        for(j=-10*(r+R);j<=10*(r+R);j++){
+            if((R-pow(pow(i/10.0,2)+pow(j/10.0,2),0.5),2>=0)&&(r*r-pow(R-pow(pow(i/10.0,2)+pow(j/10.0,2),0.5),2)>=0)){
+                    fprintf(dat,"%f,%f,%f\n",i/10.0,j/10.0,pow(r*r-pow(R-pow(pow(i/10.0,2)+pow(j/10.0,2),0.5),2), 0.5));
             }
             else{
                     fprintf(dat,"%f,%f,%f\n",i/10.0,j/10.0,0.0);
